c3b64ef24412531bbacc29451594ea8be13d3a81
Mahmoud-123Makhaly <mahmoudmakhaly123@gmail.com>
2025-05-24 10:32:06 -0800

    -practice responsive using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <div className="container mt-4 ">
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
           <ProductCard />
           <ProductCard />
           <ProductCard />
