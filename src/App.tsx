01c595653e933c48b441f11e67f12db2a8c9dcfa
Mahmoud123-Makhaly <mahmoud.abdelfatah@5d-agency.com>
2025-05-25 15:16:19 +0300

    -add modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
+import { useState } from "react";
 import ProductCard from "./components/ProductCard";
 import { productList } from "./data";
+import ButtonMaker from "./components/UI/ButtonMaker";
+import ModalMaker from "./components/UI/ModalMaker";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <main>
       <div className="container mt-4 ">
+        <div className="flex items-center justify-between mb-5">
+          <h2 className="text-4xl font-bold">
+            Latest <span className="text-indigo-500">Products</span>
+          </h2>
+          <ButtonMaker
+            className="  bg-indigo-500"
+            width="w-fit"
+            onClick={() => setIsOpen(true)}
+          >
+            Build Now
+          </ButtonMaker>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
           {productList.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
       </div>
+      <ModalMaker
+        isOpen={isOpen}
+        toggle={() => setIsOpen(!isOpen)}
+        title="Add Product"
+      >
+        <h3>efefeff</h3>
+        <div className="flex items-center  gap-2">
+          <ButtonMaker
+            className="  bg-indigo-500 hover:bg-indigo-600"
+            width="w-full"
+          >
+            Submit
+          </ButtonMaker>
+          <ButtonMaker
+            className=" bg-gray-400 hover:bg-gray-400"
+            onClick={() => setIsOpen(false)}
+            width="w-full"
+          >
+            Cancel
+          </ButtonMaker>
+        </div>
+      </ModalMaker>
     </main>
   );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import type { IProduct } from "../interfaces";
 import { TextSlicer } from "../utils/Functions";
 import ButtonMaker from "./UI/ButtonMaker";
 import ImageMaker from "./UI/ImageMaker";
+
 interface IProductCardProps {
   product: IProduct;
 }
@@ -9,6 +10,7 @@ const ProductCard = (props: IProductCardProps) => {
   const {
     product: { title, description, imageURL, price, category },
   } = props;
+
   return (
     <div className="border rounded-lg p-2 flex flex-col gap-4 max-w-sm md:max-w-md mx-auto">
       <div className="rounded-lg overflow-hidden h-[220px]">
